15283d0fccca59ecd8bcf2bd0486b57661693742
CedGarcia <icedgarcia@gmail.com>
2022-02-11 02:01:55 +0800

    Fix Get image request

--- backend/routes/image.js
+++ backend/routes/image.js
@@ -17,35 +17,34 @@ const crypto = require("crypto");
 const path = require("path");
 
 let gfs
-conn.once('open', () => {
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection("uploads");
+mongoose.connection.on("connected", () => {
+  var client = mongoose.connections[0].client;
+  var db = mongoose.connections[0].db;
+  gfs = new mongoose.mongo.GridFSBucket(db, {
+    bucketName: "uploads"
+  });
+  // console.log(bucket);
+});
 
-})
 
 // Storage Engine
 const storage = new GridFsStorage({
   url: url,
   file: (req, file) => {
     return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err)
-        }
-        const filename = buf.toString('hex') + path.extname(file.originalname);
-        const fileInfo = {
-          filename: filename,
-          bucketName: 'uploads'
-        }
-        resolve(fileInfo)
-      })
+      const filename = file.originalname;
+      const fileInfo = {
+        filename: filename,
+        bucketName: "uploads"
+      };
+      resolve(fileInfo);
+      
     })
   }
 })
 const upload = multer({ storage })
 
-
-
+// POST IMAGE
 router.post('/', upload.single('image'), async (req, res) => {
   if (req.file.contentType === 'image/jpeg' ||
     req.file.contentType === 'image/jpg' ||
@@ -55,29 +54,18 @@ router.post('/', upload.single('image'), async (req, res) => {
   else {
     return res.json({ msg: 'Invalid Image' })
   }
-
 })
-
-router.get("/:id", async (req, res) => {
-  const id = mongoose.Types.ObjectId(req.params.id);
-  gfs.files.findOne({ _id: id }, (err, file) => {
-    if (!file) {
-      return res.json({ msg: "Invalid image" });
-    } else {
-      if (
-        file.contentType === "image/jpeg" ||
-        file.contentType === "image/png" ||
-        file.contentType === "image/jpg"
-      ) {
-        const readstream = gfs.createReadStream(file.filename);
-        readstream.pipe(res);
-      }
-    }
+// GET IMAGE ROUTE
+router.get('/:id', ({ params: { id } }, res) => {
+  if (!id || id === 'undefined') return res.status(400).send('no image id');
+  const _id = new mongoose.Types.ObjectId(id);
+  gfs.find({ _id }).toArray((err, files) => {
+    if (!files || files.length === 0)
+      return res.status(400).send('no files exist');
+    gfs.openDownloadStream(_id).pipe(res);
   });
 });
-
-
-
+  
 
 
 module.exports = router
